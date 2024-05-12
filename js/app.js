d2535060e858e9be02d46ba0d3456efa397d8dbe
Arturo Hdz. Garza <arturo.hdzgarza@gmail.com>
2024-05-12 13:33:42 -0600

    Edit class Budget / app.js

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const gasList = document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners() {
   document.addEventListener('DOMContentLoaded', askBudget);
+  form.addEventListener('submit', addExpense);
 }
 
 //* Classes
@@ -16,6 +17,11 @@ class Budget {
     this.budget = Number(budget);
     this.budgetLeft = Number(budget);
     this.expenses = [];
+  }
+
+  addExpense(expense) {
+    this.expenses = [...this.expenses, expense];
+    this.budgetLeft -= expense.amount;
   }
 };
 
@@ -26,6 +32,24 @@ class UI {
     document.querySelector('#total').textContent = budget;
     document.querySelector('#restante').textContent = budgetLeft;
   }
+
+  insertAlert(message, alert) {
+    const messageDIV = document.createElement('DIV');
+    messageDIV.classList.add('text-center', 'alert');
+
+    if (alert === 'error') {
+      messageDIV.classList.add('alert-danger');
+    } else {
+      messageDIV.classList.add('alert-success');
+    }
+
+    messageDIV.textContent = message;
+    document.querySelector('.primario').insertBefore(messageDIV, form);
+
+    setTimeout(() => {
+      messageDIV.remove();
+    }, 3000);
+  };
 };
 
 //* Instances
@@ -46,3 +70,30 @@ function askBudget() {
   budget = new Budget(askUser);
   ui.insertBudget(budget);
 }
+
+function addExpense(e) {
+  e.preventDefault();
+
+  const description = document.querySelector('#gasto').value;
+  const amount = Number(document.querySelector('#cantidad').value);
+
+  if (description === '' || amount == '') {
+    ui.insertAlert('Ambos campos son obligatorios', 'error');
+    return;
+  } else if (isNaN(amount) || amount <= 0) {
+    ui.insertAlert('La cantidad debe ser un número mayor a 0', 'error');
+    return;
+  }
+
+  // Add new expense
+  const expense = { description, amount, id: Date.now() };
+  budget.addExpense(expense);
+  ui.insertAlert('Gasto agregado', 'success');
+  form.reset();
+
+  ui.insertExpense(expense);
+  ui.insertBudget(budget);
+
+  document.querySelector('#gasto').value = '';
+  document.querySelector('#cantidad').value = '';
+};
